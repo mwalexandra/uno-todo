9725324bd7a7c4ad564ca6d8416e0a4d32ed3d6b
Alexandra <mwalexandra@users.github.com>
2022-08-26 10:36:25 +0200

    startPage light thema finished

--- src/components/StartPage/index.js
+++ src/components/StartPage/index.js
@@ -7,21 +7,44 @@ import style from './index.module.css'
 function StartPage(){
 
   const mode = useSelector(state => state.interface.settings.mode)
+  const theme = useSelector(state => state.interface.settings[mode])
   
   return(
-    <section className={style.startPage}>
-      <div className={`${style.startSideBar} ${mode === 'light' ? style.light : style.dark}`}>
-        <div className={style.logo}>
-          {/* <img src="../../img/uno_platform_logo.png" alt="Logo" /> */}
-        </div>
+    <section 
+      className={style.startPage}
+      style={{
+        backgroundColor: theme.primaryColorOpacity,
+      }}  
+    >
+      <div 
+        className={style.startSideBar}
+        style={{
+          backgroundColor: theme.onPrimaryColor,
+        }}  
+      >
+        <div className={style.logo}></div>
 
         <div className={style.textBox}>
-          <h1>Welcome to Uno To Do!</h1>
-          <p>Start using the best to-do app, you can create and manage your To Do lists to improve your organisation.</p>
+          <h1
+            style={{
+              color: theme.onSurfaceColor,
+            }} 
+          >Welcome to Uno To Do!</h1>
+          <p
+            style={{
+              color: theme.primaryVariantDarkColor,
+            }}  
+          >Start using the best to-do app, you can create and manage your To Do lists to improve your organisation.</p>
         </div>
-        <Link to='/uno-todo' className={style.startedBtn}>Get started</Link>
+        <Link 
+          to='/uno-todo' 
+          className={style.startedBtn}
+          style={{
+            color: theme.onPrimaryColor,
+            backgroundColor: theme.primaryColor,
+          }}
+        >Get started</Link>
       </div>
-
       <div className={style.startBack}></div>
     </section>
   )
